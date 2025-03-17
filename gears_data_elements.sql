abfa034f7a02b40a04760afcb178035b38fb2ee9
Daniella Meeker <daniella.meeker@yale.edu>
2025-03-17 09:29:32 -0400

    Update gears_data_elements.sql

--- gears_data_elements.sql
+++ gears_data_elements.sql
@@ -6,4 +6,12 @@
 
 3. GENERATE DATA-ELEMENT SPECIFIC QUERIES USING "SPECIFICATIONS FOR QUERY" TAB IN gearss_data_dictionary_wOMOP.XLSX FILE
 
+
+Column Titles
+-- Table Name
+-- Source Text String (may need to join to source-to-concept tables depending on implementation) 
+-- Source Concept ID 
+-- Standard Text String (need to join to vocabulary tables)
+-- Standard Concept ID
+-- Count of this source-standard pair in ED encounters
 */
